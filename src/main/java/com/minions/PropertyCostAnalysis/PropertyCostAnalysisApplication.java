e8e03486de00b26461818663c3dc439efa04e8ff
Jaseel <harisj@uwindsor.ca>
2024-02-25 01:56:22 -0500

    init repo

---
+++ src/main/java/com/minions/PropertyCostAnalysis/PropertyCostAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.minions.PropertyCostAnalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyCostAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyCostAnalysisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minions/PropertyCostAnalysis/PropertyCostAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minions.PropertyCostAnalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyCostAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
