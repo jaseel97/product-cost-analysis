a7867b8fea8e0af10edaae498a829a87ee25926b
jaseel <harisj@uwindsor.ca>
2024-03-31 18:23:30 -0400

    replace old files with maven setup

---
+++ src/main/java/SearchFrequency.java
@@ -0,0 +1,97 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+
+public class SearchFrequency {
+
+    private static final String JSON_FILE_PATH = "C:\\Users\\royce\\eclipse-workspace\\ACC_Final_Project\\RealtorProperties.json";
+
+    public static void main(String[] args) {
+        // Load JSON data
+//        JSONArray properties = loadJSONData(JSON_FILE_PATH);
+//
+//        Scanner scanner = new Scanner(System.in);
+//        String searchTerm;
+//        
+//        do {
+//            System.out.print("Enter a city name (type 'exit' to exit): ");
+//            searchTerm = scanner.nextLine().trim().toLowerCase(); // Convert to lowercase
+//            if (!searchTerm.equalsIgnoreCase("exit")) {
+//                updateSearchFrequency(properties);
+//                saveJSONData(JSON_FILE_PATH, properties);
+//            }
+//        } while (!searchTerm.equalsIgnoreCase("exit"));
+    }
+
+//    private static JSONArray loadJSONData(String filename) {
+//        JSONParser parser = new JSONParser();
+//        JSONArray jsonArray = new JSONArray();
+//        try (FileReader fileReader = new FileReader(filename)) {
+//            Object obj = parser.parse(fileReader);
+//            jsonArray = (JSONArray) obj;
+//        } catch (IOException | ParseException e) {
+//            e.printStackTrace();
+//        }
+//        return jsonArray;
+//    }
+
+    private static void updateSearchFrequency(JsonObject propertyDetails) {
+    	int frequency = propertyDetails.get("search_frequency").getAsInt();
+        propertyDetails.addProperty("search_frequency", frequency + 1);
+        
+        // Get the name of the propertyDetails
+        String propertyName = propertyDetails.get("propertyName").getAsString();
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        try (FileReader reader = new FileReader(JSON_FILE_PATH)) {
+            // Parse JSON file into JsonArray
+            JsonArray propertiesArray = JsonParser.parseReader(reader).getAsJsonArray();
+            if (propertiesArray != null) {
+                // Iterate through each property in the array
+                for (JsonElement element : propertiesArray) {
+                    JsonObject property = element.getAsJsonObject();
+                    // Check if the "propertyName" of the property matches the name of propertyDetails
+                    if (property.get("propertyName").getAsString().equals(propertyName)) {
+                        // Update search frequency in the JsonObject
+                        property.addProperty("search_frequency", propertyDetails.get("search_frequency").getAsInt());
+                        break; // Exit loop after updating
+                    }
+                }
+            }
+
+            // Write updated JsonArray back to the JSON file
+            try (FileWriter writer = new FileWriter(JSON_FILE_PATH)) {
+                gson.toJson(propertiesArray, writer);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+//    private static void saveJSONData(String filename, List <PropertyDetails> propertiesList) {
+//        try (FileWriter fileWriter = new FileWriter(filename)) {
+//        	((JSONArray) propertiesList).writeJSONString(fileWriter);
+//            fileWriter.flush();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+    
+    public static void call(JsonObject propertyDetail) {
+    	updateSearchFrequency(propertyDetail);
+    }
+}
---
+++ src/main/java/ZoloDataFormatter.java
@@ -0,0 +1,253 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+public class ZoloDataFormatter {
+	public static void main(String[] args) {
+		Gson gson = new Gson();
+
+		try {
+			//parse JSON array of objects
+			JsonArray jsonArray = gson.fromJson(new FileReader("zolo.json"), JsonArray.class);
+			//create a list to hold PropertyDetails objects
+			List<PropertyDetails> propertyList = new ArrayList<>();
+			//iterate over JSON objects
+			for (JsonElement element : jsonArray) {
+				JsonObject jsonObject = element.getAsJsonObject();
+				//iterate through the different elements - this part. Now the whole properties per listing per city is formatted
+				for (Map.Entry<String, JsonElement> entry : jsonObject.entrySet()) {
+					String jsonKeyValue = entry.getKey();
+					JsonElement propertyValue = entry.getValue();
+					String PropertyValueString = propertyValue.toString();
+					String replacedPropertyValueString = PropertyValueString.replaceAll("\\\\n", "|");
+					String propertyName = FindPropertyNameFromString(replacedPropertyValueString);
+					BigDecimal propertyPrice = FindPropertyPriceFromString(replacedPropertyValueString);
+					String pincode = FindPropertyPincodeFromString(replacedPropertyValueString);
+					String mlsNumber = FindMLSNumberIdentifier(replacedPropertyValueString);
+					int bedrooms = getBedroomsFromListing(replacedPropertyValueString);
+					int bathrooms = getBathroomsFromListing(replacedPropertyValueString);
+					String description = getPropertyDescription(replacedPropertyValueString);
+					String buildingType = getPropertyBuildingType(replacedPropertyValueString);
+					String propertyCity = getPropertyCity(jsonKeyValue.toString());
+					String province = getPropertyProvince(jsonKeyValue.toString());
+					// Create PropertyDetails object and add it to the list
+					PropertyDetails propertyDetails = new PropertyDetails(mlsNumber, propertyName, buildingType, propertyCity, province, pincode, propertyPrice, bedrooms, bathrooms, description, 0);
+					propertyList.add(propertyDetails);
+				}
+			}
+
+			//Write propertyList to JSON file
+			Gson gsonBuilder = new GsonBuilder().setPrettyPrinting().create();
+			try (FileWriter writer = new FileWriter("ZoloProperties.json")) {
+				gsonBuilder.toJson(propertyList, writer);
+				System.out.println("Property details written to ZoloProperties.json successfully.");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static String FindPropertyNameFromString(String jsonStringValue) {
+		String propertyName = null;
+		Pattern pattern = Pattern.compile("^\"?(.*?)\\|");
+
+		// Create a matcher with the long string
+		Matcher matcher = pattern.matcher(jsonStringValue);
+
+		// Find the address
+		if (matcher.find()) {
+			propertyName = matcher.group(1).trim(); // Group 1 captures the address
+		}
+		return propertyName;
+	}
+
+	private static BigDecimal FindPropertyPriceFromString(String jsonStringValue) {
+        String patternString = "\\|\\$([\\d,]+)\\s";
+        Pattern pattern = Pattern.compile(patternString);
+        Matcher matcher = pattern.matcher(jsonStringValue);
+        BigDecimal price = null;
+
+        // Find and print the first match (if any)
+        if (matcher.find()) {
+            String priceStr = matcher.group(1).replaceAll(",", "");
+            price = new BigDecimal(priceStr);
+        }
+        return price;
+    }
+
+	private static String FindPropertyPincodeFromString(String jsonStringValue) {
+		String pattern = "[A-Za-z]\\d[A-Za-z]\\s?\\d[A-Za-z]\\d";
+		Pattern postalCodePattern = Pattern.compile(pattern);
+		Matcher matcher = postalCodePattern.matcher(jsonStringValue);
+		String postalCode = null;
+		if(matcher.find()) {
+			postalCode = matcher.group();
+		}
+
+		return postalCode;
+	}
+
+	private static String FindMLSNumberIdentifier(String jsonStringValue) {
+		String patternString = "MLS�#\\|([A-Za-z0-9]+)\\|";
+		Pattern mlsPattern = Pattern.compile(patternString);
+		Matcher matcher = mlsPattern.matcher(jsonStringValue);
+		String mlsNumber = null;
+
+		if (matcher.find()) {
+			mlsNumber = matcher.group(1);
+		}
+		return mlsNumber;
+	}
+
+	private static int getBedroomsFromListing(String jsonStringValue) {
+		String extractedData = null;
+		String pattern = "\\|(\\d+\\+\\d+|\\d+)\\s+bed\\s"; // Regex pattern to match "1+1" followed by "bed" and space
+		Pattern postalCodePattern = Pattern.compile(pattern);
+		Matcher matcher = postalCodePattern.matcher(jsonStringValue);
+
+		if(matcher.find()) {
+			extractedData = matcher.group(1);
+		}
+
+		if (extractedData == null) {
+			return 0; // Return 0 if extractedData is null
+		}
+
+		int sum = 0;
+		if (extractedData != null && extractedData.contains("+")) {
+			String[] parts = extractedData.split("\\+");
+			for (String part : parts) {
+				sum += Integer.parseInt(part.trim());
+			}
+		} else {
+			return Integer.parseInt(extractedData.trim());
+		}
+
+		return sum;
+	}
+
+	private static int getBathroomsFromListing(String jsonStringValue) {
+		String extractedData = null;
+		String pattern = "\\s(\\d+)\\s+bath\\s";
+		Pattern postalCodePattern = Pattern.compile(pattern);
+		Matcher matcher = postalCodePattern.matcher(jsonStringValue);
+		
+		if(matcher.find()) {
+			extractedData = matcher.group(1);
+		}
+		
+		if (extractedData == null) {
+			return 0; // Return 0 if extractedData is null
+		}
+		
+		int sum = 0;
+		if (extractedData.contains("+")) {
+			String[] parts = extractedData.split("\\+");
+			for (String part : parts) {
+				sum += Integer.parseInt(part.trim());
+			}
+		} else {
+			return Integer.parseInt(extractedData.trim());
+		}
+
+		return sum;
+	}
+
+	private static String getPropertyDescription(String jsonStringValue) {
+		String propertyDescription = null;
+		String patternString = "Listed By(?:.*?\\|){2}(.*?)(?:\\||$)";
+		Pattern pattern = Pattern.compile(patternString);
+		Matcher matcher = pattern.matcher(jsonStringValue);
+
+		if (matcher.find()) {
+			propertyDescription = matcher.group(1).trim();
+		}
+		return propertyDescription;
+	}
+
+	private static String getPropertyBuildingType(String jsonStringValue) {
+		String buildingType = null;
+		String patternString = "Style\\|(.*?)\\|";
+		Pattern pattern = Pattern.compile(patternString);
+		Matcher matcher = pattern.matcher(jsonStringValue);
+
+		if (matcher.find()) {
+			buildingType = matcher.group(1).trim();
+		}
+		return buildingType;
+	}
+
+//	private static float getNumberOfStoreys(String jsonStringValue) {
+//		float numberOfStoreys = 0;
+//		Pattern pattern = Pattern.compile("Storeys\\\\n(.+?)\\\\n");
+//		Matcher matcher = pattern.matcher(jsonStringValue);
+//
+//		if (matcher.find()) {
+//			String storeys = matcher.group(1).trim();
+//			numberOfStoreys = Float.parseFloat(storeys);
+//		}
+//		return numberOfStoreys;
+//	}
+//
+//	private static double getSqftAreaOfProperty(String jsonStringValue) {
+//		double propertyArea = 0;
+//		Pattern pattern = Pattern.compile("\\\\n([0-9.]+)\\s*sqft");
+//		Matcher matcher = pattern.matcher(jsonStringValue);
+//		if (matcher.find()) {
+//			String areaMeasurementString = matcher.group(1).trim();
+//			propertyArea = Double.parseDouble(areaMeasurementString);
+//		} 
+//		return propertyArea;
+//	}
+
+	private static String getPropertyCity(String jsonStringValue) {
+		String propertyCity = null;
+		String[] partsOfString = jsonStringValue.split(",");
+		if (partsOfString.length > 0) {
+			propertyCity = partsOfString[0].trim();
+		}
+		return propertyCity;
+	}
+
+	private static String getPropertyProvince(String jsonStringValue) {
+		// Extracting the province code from the input string
+        String provinceCode = jsonStringValue.substring(jsonStringValue.lastIndexOf(',') + 2, jsonStringValue.lastIndexOf('_'));
+
+        // Mapping the province code to province name
+        String provinceName = mapProvinceCode(provinceCode);
+
+        return provinceName;
+	}
+	
+	private static String mapProvinceCode(String provinceCode) {
+        // Mapping of province codes to province names
+        switch (provinceCode) {
+            case "ON":
+                return "Ontario";
+            case "QC":
+                return "Quebec";
+            case "AB":
+                return "Alberta";
+            case "BC":
+                return "British Columbia";
+            case "NS":
+                return "Nova Scotia";
+            default:
+                return "Unknown"; // Default to "Unknown" if no mapping is found
+        }
+    }
+}
