f4bd90e76e26a1208deaad880b35245431681989
smol <sanjivk@uwindsor.ca>
2024-04-01 00:54:52 -0400

    add back option for main menu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,6 +13,8 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 public class Main {
+	
+	static final String BACK="!b";
 
 	public static void main(String[] args) {
 		System.out.println("\n\n------------------------------------- Property Lens -----------------------------------------------------");
@@ -29,6 +31,8 @@ public class Main {
 
 		InvertedIndex index = new InvertedIndex();
 		index.buildIndexFromJSON(propertyList);
+		
+		//
 
 		Scanner inputReader = new Scanner(System.in);
 		String searchOption;
@@ -44,8 +48,10 @@ public class Main {
 
 			switch (searchOption) {
 			case "1":
-				System.out.println("\nEnter city:");
+				System.out.println("\nEnter city: (Type "+BACK+" to go back to main menu)");
 				String searchedCity = inputReader.nextLine();
+				if(searchedCity.equals(BACK))
+					break;
 				boolean isValidInput = validateStringInput(searchedCity);
 				if(isValidInput)
 					searchByProperty(propertyList, searchedCity, "city");
@@ -53,8 +59,10 @@ public class Main {
 					System.out.println("Invalid city name. Please enter a valid city with letters only.");
 				break;
 			case "2":
-				System.out.println("\nEnter province:");
+				System.out.println("\nEnter province:  (Type "+BACK+" to go back to main menu)");
 				String searchedProvince = inputReader.nextLine();
+				if(searchedProvince.equals(BACK))
+					break;
 				boolean isValidInput2 = validateStringInput(searchedProvince);
 				if(isValidInput2)
 					searchByProperty(propertyList, searchedProvince, "province");
@@ -62,8 +70,10 @@ public class Main {
 					System.out.println("Invalid province name. Please enter a valid province with letters only.");
 				break;
 			case "3":
-				System.out.println("\nEnter pincode:");
+				System.out.println("\nEnter pincode: (Type "+BACK+" to go back to main menu)");
 				String searchedPincode = inputReader.nextLine();
+				if(searchedPincode.equals(BACK))
+					break;
 				boolean isValidInput3 = validatePincodeInput(searchedPincode);
 				if(isValidInput3) {
 					searchedPincode = searchedPincode.replace(" ", "");
@@ -73,8 +83,10 @@ public class Main {
 					System.out.println("Invalid Pincode. Please enter a valid Canadian postal code!");
 				break;
 			case "4":
-				System.out.println("\nEnter the lower price range:");
+				System.out.println("\nEnter the lower price range: (Type "+BACK+" to go back to main menu)");
 				String lowerPriceString = inputReader.nextLine();
+				if(lowerPriceString.equals(BACK))
+					break;
 				boolean isValidInput4 = validatePriceInput(lowerPriceString);
 				if(isValidInput4) {
 					System.out.println("\nEnter the higher price range:");
@@ -91,8 +103,10 @@ public class Main {
 					System.out.println("Invalid price format. Please enter a valid price in numbers only! (Decimal points can be used)");
 				break;
 			case "5":
-				System.out.println("\nEnter city:");
+				System.out.println("\nEnter city: (Type "+BACK+" to go back to main menu)");
 				String searchedCity2 = inputReader.nextLine();
+				if(searchedCity2.equals(BACK))
+					break;
 				boolean isCityValidInput = validateStringInput(searchedCity2);
 				if(isCityValidInput) {
 					System.out.println("\nEnter the lower price range:");
@@ -158,60 +172,60 @@ public class Main {
 				}
 				else
 					System.out.println("No property found with the provided "+searchFactor+"!");
-	        } else {
-	        	System.out.print("Did you mean: ");
-	        	//To print word alone if single suggestion
-	        	if (autoCompletedWords.size() == 1) {
-	        	    String completedWord = autoCompletedWords.get(0);
-	        	    System.out.println(completedWord.substring(0, 1).toUpperCase() + completedWord.substring(1));
-	        	} else {
-	        		// to print words separated by a comma if multiple suggestions are present
-	        	    for (int i = 0; i < autoCompletedWords.size(); i++) {
-	        	        String completedWord = autoCompletedWords.get(i);
-	        	        System.out.print(completedWord.substring(0, 1).toUpperCase() + completedWord.substring(1));
-	        	        
-	        	        // If it's not the last word, print a comma
-	        	        if (i < autoCompletedWords.size() - 1) {
-	        	            System.out.print(", ");
-	        	        }
-	        	    }
-	        	}
-	        }
+			} else {
+				System.out.print("Did you mean: ");
+				//To print word alone if single suggestion
+				if (autoCompletedWords.size() == 1) {
+					String completedWord = autoCompletedWords.get(0);
+					System.out.println(completedWord.substring(0, 1).toUpperCase() + completedWord.substring(1));
+				} else {
+					// to print words separated by a comma if multiple suggestions are present
+					for (int i = 0; i < autoCompletedWords.size(); i++) {
+						String completedWord = autoCompletedWords.get(i);
+						System.out.print(completedWord.substring(0, 1).toUpperCase() + completedWord.substring(1));
+
+						// If it's not the last word, print a comma
+						if (i < autoCompletedWords.size() - 1) {
+							System.out.print(", ");
+						}
+					}
+				}
+			}
 		}
 	}
-	
+
 	public static void searchByCityPriceBedroomsAndBathrooms(JsonArray propertyList, String cityInput, String lowerPriceString2, String higherPriceString, String noOfBedrooms, String noOfBathrooms) {
-        int propertyCount = 0;
-        BigDecimal lowerPriceRangeValue = new BigDecimal(lowerPriceString2);
-        BigDecimal higherPriceRangeValue = new BigDecimal(higherPriceString);
-        int bedroomsInputValue = Integer.parseInt(noOfBedrooms);
-        int bathroomsInputValue = Integer.parseInt(noOfBathrooms);
-        for (JsonElement element : propertyList) {
-            JsonObject property = element.getAsJsonObject();
-            JsonElement cityElement = property.get("city");
-            JsonElement priceElement = property.get("price");
-            JsonElement bedroomsElement = property.get("bedrooms");
-            JsonElement bathroomsElement = property.get("bathrooms");
-            
-            if (cityElement != null && cityElement.getAsString().equalsIgnoreCase(cityInput)) {
-                BigDecimal price = priceElement.getAsBigDecimal();
-                int bedrooms = bedroomsElement.getAsInt();
-                int bathrooms = bathroomsElement.getAsInt();
-                
-                // Check if the price is within the specified range
-                if (price.compareTo(lowerPriceRangeValue) >= 0 && price.compareTo(higherPriceRangeValue) <= 0) {
-                    // Check if the number of bedrooms and bathrooms match the specified criteria
-                    if (bedrooms == bedroomsInputValue && bathrooms == bathroomsInputValue) {
-                    	SearchFrequency.call(property);
-                        DisplayFormatter.printPropertyDetails(property);
-                        propertyCount++;
-                    }
-                }
-            } 
-        }
-        if(propertyCount == 0) 
-            System.out.println("No properties found with the provided criteria!");
-    }
+		int propertyCount = 0;
+		BigDecimal lowerPriceRangeValue = new BigDecimal(lowerPriceString2);
+		BigDecimal higherPriceRangeValue = new BigDecimal(higherPriceString);
+		int bedroomsInputValue = Integer.parseInt(noOfBedrooms);
+		int bathroomsInputValue = Integer.parseInt(noOfBathrooms);
+		for (JsonElement element : propertyList) {
+			JsonObject property = element.getAsJsonObject();
+			JsonElement cityElement = property.get("city");
+			JsonElement priceElement = property.get("price");
+			JsonElement bedroomsElement = property.get("bedrooms");
+			JsonElement bathroomsElement = property.get("bathrooms");
+
+			if (cityElement != null && cityElement.getAsString().equalsIgnoreCase(cityInput)) {
+				BigDecimal price = priceElement.getAsBigDecimal();
+				int bedrooms = bedroomsElement.getAsInt();
+				int bathrooms = bathroomsElement.getAsInt();
+
+				// Check if the price is within the specified range
+				if (price.compareTo(lowerPriceRangeValue) >= 0 && price.compareTo(higherPriceRangeValue) <= 0) {
+					// Check if the number of bedrooms and bathrooms match the specified criteria
+					if (bedrooms == bedroomsInputValue && bathrooms == bathroomsInputValue) {
+						SearchFrequency.call(property);
+						DisplayFormatter.printPropertyDetails(property);
+						propertyCount++;
+					}
+				}
+			} 
+		}
+		if(propertyCount == 0) 
+			System.out.println("No properties found with the provided criteria!");
+	}
 
 	public static void searchByPriceRange(JsonArray propertyList, BigDecimal lowerPriceInput, BigDecimal higherPriceInput) {
 		int propertyCount = 0;
@@ -256,12 +270,12 @@ public class Main {
 		Matcher matcher = pattern.matcher(userInputPrice);
 		return matcher.matches();
 	}
-	
+
 	public static boolean isValidInteger(String input) {
-        // Regular expression to match an integer (positive or negative)
-        String regex = "[-+]?\\d+";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(input);
-        return matcher.matches();
-    }
+		// Regular expression to match an integer (positive or negative)
+		String regex = "[-+]?\\d+";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(input);
+		return matcher.matches();
+	}
 }
