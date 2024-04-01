664c70c66dd781835aeecc0d6d3ecbf296cf2a31
Parvathi Joshi <parvathijoshi@Parvathis-MacBook-Pro.local>
2024-04-01 10:42:52 -0400

    Updated SearchFrequency file to include HashMap

--- src/main/java/SearchFrequency.java
+++ src/main/java/SearchFrequency.java
@@ -8,54 +8,52 @@ import com.google.gson.JsonParser;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SearchFrequency {
 
     private static final String JSON_FILE_PATH = "src/main/resources/CombinedProperties.json";
+    private static final Map<String, JsonObject> propertyMap = new HashMap<>();
 
-    private static void updateSearchFrequency(JsonObject propertyDetails) {
-    	int frequency = propertyDetails.get("search_frequency").getAsInt();
-        propertyDetails.addProperty("search_frequency", frequency + 1);
-        
-        // Get the name of the propertyDetails
-        String propertyName = propertyDetails.get("propertyName").getAsString();
+    static {
+        loadPropertyDetails();
+    }
 
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    private static void loadPropertyDetails() {
         try (FileReader reader = new FileReader(JSON_FILE_PATH)) {
-            // Parse JSON file into JsonArray
             JsonArray propertiesArray = JsonParser.parseReader(reader).getAsJsonArray();
-            if (propertiesArray != null) {
-                // Iterate through each property in the array
-                for (JsonElement element : propertiesArray) {
-                    JsonObject property = element.getAsJsonObject();
-                    // Check if the "propertyName" of the property matches the name of propertyDetails
-                    if (property.get("propertyName").getAsString().equals(propertyName)) {
-                        // Update search frequency in the JsonObject
-                        property.addProperty("search_frequency", propertyDetails.get("search_frequency").getAsInt());
-                        break; // Exit loop after updating
-                    }
-                }
-            }
-
-            // Write updated JsonArray back to the JSON file
-            try (FileWriter writer = new FileWriter(JSON_FILE_PATH)) {
-                gson.toJson(propertiesArray, writer);
+            for (JsonElement element : propertiesArray) {
+                JsonObject property = element.getAsJsonObject();
+                String propertyName = property.get("propertyName").getAsString();
+                propertyMap.put(propertyName, property);
             }
         } catch (IOException e) {
-            System.out.println("Error writing search frequency to json");
+            System.out.println("Error loading property details from JSON file");
         }
     }
 
-//    private static void saveJSONData(String filename, List <PropertyDetails> propertiesList) {
-//        try (FileWriter fileWriter = new FileWriter(filename)) {
-//        	((JSONArray) propertiesList).writeJSONString(fileWriter);
-//            fileWriter.flush();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-    
+    private static void updateSearchFrequency(JsonObject propertyDetails) {
+        int frequency = propertyDetails.get("search_frequency").getAsInt();
+        propertyDetails.addProperty("search_frequency", frequency + 1);
+
+        String propertyName = propertyDetails.get("propertyName").getAsString();
+        propertyMap.put(propertyName, propertyDetails);
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        try (FileWriter writer = new FileWriter(JSON_FILE_PATH)) {
+            JsonArray propertiesArray = new JsonArray();
+            for (JsonObject property : propertyMap.values()) {
+                propertiesArray.add(property);
+            }
+            gson.toJson(propertiesArray, writer);
+            
+        } catch (IOException e) {
+            System.out.println("Error writing search frequency to JSON file");
+        }
+    }
+
     public static void call(JsonObject propertyDetail) {
-    	updateSearchFrequency(propertyDetail);
+        updateSearchFrequency(propertyDetail);
     }
 }
